2a32d8c7c5c4618acee0c4bbda5a565e854e697e
renesas-michael-thomas <52974986+michaelthomasj@users.noreply.github.com>
2025-11-05 14:32:58 -0500

    Add RTOS based secure project

---
+++ FSP_Project_ra6m4_s_rtos/ra_cfg/fsp_cfg/rm_tz_context_cfg.h
@@ -0,0 +1,14 @@
+/* generated configuration header file - do not edit */
+#ifndef RM_TZ_CONTEXT_CFG_H_
+#define RM_TZ_CONTEXT_CFG_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define TZ_PROCESS_STACK_SLOTS 8
+#define TZ_PROCESS_STACK_SIZE 256
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* RM_TZ_CONTEXT_CFG_H_ */
